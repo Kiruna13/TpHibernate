929a9a0ed0b6b5bbf7aa9e177e7d5dfe49c24539
lexraccoon <laura.goncalves1@epsi.fr>
2022-06-21 16:40:06 +0200

    add client

--- src/main/java/Queries/NouveauClient.java
+++ src/main/java/Queries/NouveauClient.java
@@ -1,4 +1,34 @@
 package Queries;
 
+import Entities.Client;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class NouveauClient {
+    public static void main(String[] args) {
+
+        SessionFactory factory = HibernateUtils.getSessionFactory();
+
+        Session session = factory.getCurrentSession();
+        Client client = null;
+        
+        try {
+            session.getTransaction().begin();
+
+            client = new Client();
+            client.setNom("Milos");
+            client.setPrenom("Ricardo");
+            client.setTelephone("0636426996");
+
+            session.persist(client);
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
+        assert client != null;
+        System.out.println("Nom dernier client: " + client.getNom());
+    }
 }
