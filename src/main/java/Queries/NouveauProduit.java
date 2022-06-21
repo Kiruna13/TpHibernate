ec59c9bffba4ab364466bed9291c1db3a01338ab
Thomas Ducoulombier <thomas.ducoulombier@free.fr>
2022-06-21 16:43:31 +0200

    Rename class produit

--- src/main/java/Queries/NouveauProduit.java
+++ src/main/java/Queries/NouveauProduit.java
@@ -10,7 +10,7 @@ import org.hibernate.SessionFactory;
 
 public class NouveauProduit {
 
-    public static void main(String[] args) {
+    public static void addProduit(String[] args) {
 
         SessionFactory factory = HibernateUtils.getSessionFactory();
 
