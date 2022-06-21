7a3ffa2ee49c3f8acc4ac066fe72c6fbc1d8c941
Thomas Ducoulombier <thomas.ducoulombier@free.fr>
2022-06-21 16:42:37 +0200

    Suppr Comments

--- src/main/java/Queries/NouveauProduit.java
+++ src/main/java/Queries/NouveauProduit.java
@@ -20,30 +20,18 @@ public class NouveauProduit {
         try {
             session.getTransaction().begin();
 
-            // Create transient object
             produit = new Produit();
             produit.setNom("Zbigznah32Z");
             produit.setPrix(12.5f);
 
-
-            // Using persist(..)
-            // Now 'emp' is managed by Hibernate.
-            // it has Persistent status.
-            // No action at this time with DB.
             session.persist(produit);
 
-
-            // At this step the data is pushed to the DB.
-            // Execute Insert statement.
             session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
             session.getTransaction().rollback();
         }
 
-        // After the session is closed (commit, rollback, close)
-        // Objects 'emp', 'dept' became the Detached objects.
-        // It is no longer in the control of the session.
         System.out.println("Produit No: " + produit.getIdproduit());
 
     }
