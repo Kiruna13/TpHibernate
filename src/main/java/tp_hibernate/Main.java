a83714c92917416c4dff1f20d3abd2e460957108
lexraccoon <laura.goncalves1@epsi.fr>
2022-06-23 11:00:47 +0200

    add first select commande

--- src/main/java/tp_hibernate/Main.java
+++ src/main/java/tp_hibernate/Main.java
@@ -1,9 +1,10 @@
 package tp_hibernate;
 
 import tp_hibernate.Queries.NouveauProduit;
+import tp_hibernate.Queries.RequetesCommande;
 
 public class Main {
     public static void main(String[] args) {
-        new NouveauProduit().addProduit();
+        new RequetesCommande().allCommandesClient();
     }
 }
--- src/main/java/tp_hibernate/Queries/RequetesCommande.java
+++ src/main/java/tp_hibernate/Queries/RequetesCommande.java
@@ -1,4 +1,50 @@
 package tp_hibernate.Queries;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import tp_hibernate.Entities.Client;
+import tp_hibernate.Entities.Commande;
+import tp_hibernate.HibernateUtils;
+
+import java.util.List;
+
 public class RequetesCommande {
+
+    public void allCommandesClient(){
+        SessionFactory factory = HibernateUtils.getSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.getTransaction().begin();
+
+            String sql = "SELECT co.id_commande, co.date_commande, co.heure_commande FROM " + Commande.class.getName() + " co " +
+                    "INNER JOIN "+ Client.class.getName() +" cl on cl.id_client = co.client " +
+                    "WHERE cl.nom_client = :nom_client " +
+                    "OR cl.prenom_client = :prenom_client " +
+                    "OR cl.telephone_client = :telephone_client";
+
+
+            // Create Query object.
+            Query<Commande> query = session.createQuery(sql, Commande.class);
+
+            query.setParameter("nom_client", "Goncalves");
+
+            // Execute query.
+            List<Commande> commandes = query.getResultList();
+
+            for (Commande co : commandes) {
+                System.out.println("Commandes client " + co.getDateCommande() + " : "
+                        + co.getHeureCommande());
+            }
+
+            // Commit data.
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Rollback in case of an error occurred.
+            session.getTransaction().rollback();
+        }
+    }
 }
